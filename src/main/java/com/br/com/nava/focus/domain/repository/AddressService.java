879fe5d384806f570a509ba6b307b308f6226681
luger-mano <lucas.germanysantos@gmail.com>
2026-01-27 01:22:23 -0300

    [feat] : Criação de AddressService como interface visando os principios SOLID

--- src/main/java/com/br/com/nava/focus/domain/repository/AddressService.java
+++ src/main/java/com/br/com/nava/focus/domain/repository/AddressService.java
@@ -1,4 +1,11 @@
 package com.br.com.nava.focus.domain.repository;
 
-public interface AddressService {
+import com.br.com.nava.focus.adapter.dto.address.AddressRequestDto;
+import com.br.com.nava.focus.domain.model.Address;
+import com.br.com.nava.focus.domain.model.Store;
+import jakarta.validation.Valid;
+
+public interface AddressService extends ViaCepIntegration{
+
+    Address saveAddress(@Valid AddressRequestDto addressRequestDto, Store store);
 }
