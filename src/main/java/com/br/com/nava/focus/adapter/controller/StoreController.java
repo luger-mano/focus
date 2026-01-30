7eea7af401b6542a710c6cfc9da4ef5dfa6b52cd
luger-mano <lucas.germanysantos@gmail.com>
2026-01-30 04:01:25 -0300

    [feat] : Adicionei um método get novo, para buscar pelo Id do usuário com busca binária, organizando o array com insertion sort

--- src/main/java/com/br/com/nava/focus/adapter/controller/StoreController.java
+++ src/main/java/com/br/com/nava/focus/adapter/controller/StoreController.java
@@ -7,34 +7,39 @@ import com.br.com.nava.focus.adapter.dto.store.StoreResponseDto;
 import com.br.com.nava.focus.domain.model.Store;
 import com.br.com.nava.focus.domain.repository.StoreService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.repository.query.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class StoreController {
 
-
     private final StoreService storeService;
 
-
     @PostMapping("/store")
-    public ResponseEntity<Store> createStore(@RequestBody CreateStoreRequestDto requestDto){
+    public ResponseEntity<Store> createStore(@RequestBody CreateStoreRequestDto requestDto) {
         var storeCreated = storeService.createStore(requestDto);
         return ResponseEntity.ok(storeCreated);
     }
 
     @GetMapping("/stores")
     public ResponseEntity<List<StorePaginationDto>> getAllStores(@RequestParam(value = "page",
-                                                                           defaultValue = "0") int page,
+                                                                         defaultValue = "0") int page,
                                                                  @RequestParam(value = "pageSize",
-                                                                       defaultValue = "10") int pageSize){
+                                                                         defaultValue = "10") int pageSize) {
         var stores = storeService.getAllStores(page, pageSize);
         return ResponseEntity.ok(stores);
+    }
+
+    @GetMapping("/store/{id}")
+    public ResponseEntity<StoreResponseDto> getStoreById(@PathVariable UUID id) {
+        var storeId = storeService.getStoreById(id);
+        return ResponseEntity.ok(storeId);
     }
 
 }
