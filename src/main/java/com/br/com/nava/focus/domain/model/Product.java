6ed215a3b704edb4cb8ee8139b4485d2018104a2
luger-mano <lucas.germanysantos@gmail.com>
2026-02-05 01:33:25 -0300

    [refact] : atributos de product passaram para a entidade associativa de Store e Product

--- src/main/java/com/br/com/nava/focus/domain/model/Product.java
+++ src/main/java/com/br/com/nava/focus/domain/model/Product.java
@@ -13,23 +13,8 @@ import java.util.UUID;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID productId;
     private String name;
-    private Double price;
-    private String description;
-    private Boolean available;
-    private Integer amount;
-
-
-    public Product(UUID productId, String name, Double price, String description, Boolean available, Integer amount) {
-        this.productId = productId;
-        this.name = name;
-        this.price = price;
-        this.description = description;
-        this.available = available;
-        this.amount = amount;
-    }
 
     public Product() {
     }
