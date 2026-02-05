c58865876408466f1840f192dd16c773df3daa22
luger-mano <lucas.germanysantos@gmail.com>
2026-02-05 01:26:48 -0300

    [feat] : BrandController.java adicionado com dois métodos POST e GET

---
+++ src/main/java/com/br/com/nava/focus/adapter/controller/BrandController.java
@@ -0,0 +1,34 @@
+package com.br.com.nava.focus.adapter.controller;
+
+
+import com.br.com.nava.focus.adapter.dto.brand.BrandRequestDto;
+import com.br.com.nava.focus.adapter.dto.brand.BrandResponseDto;
+import com.br.com.nava.focus.domain.model.Brand;
+import com.br.com.nava.focus.domain.service.brand.BrandService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class BrandController {
+
+    private final BrandService brandService;
+
+    @PostMapping("/brand")
+    public ResponseEntity<BrandResponseDto> createBrand(@RequestBody @Valid BrandRequestDto dto) {
+        var brand = brandService.createBrand(dto);
+        return ResponseEntity.ok(brand);
+    }
+
+    @GetMapping("/{brandId}/brands")
+    public ResponseEntity<BrandResponseDto> getAllBrandsById(@PathVariable String brandId){
+        var brand = brandService.getBrandById(brandId);
+        return ResponseEntity.ok(brand);
+    }
+
+}
