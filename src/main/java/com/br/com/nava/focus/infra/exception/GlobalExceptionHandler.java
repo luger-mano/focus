3150bb35cd8821fa53bed9137bbe17e730bbc98b
luger-mano <lucas.germanysantos@gmail.com>
2026-02-05 01:31:59 -0300

    [feat] : Adicionei a classe de tratamento de exceções globais, mais especifico para lidar com token de acesso

---
+++ src/main/java/com/br/com/nava/focus/infra/exception/GlobalExceptionHandler.java
@@ -0,0 +1,65 @@
+package com.br.com.nava.focus.infra.exception;
+
+import com.nimbusds.jwt.proc.ExpiredJWTException;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.authentication.AccountStatusException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.AccessDeniedException;
+import java.security.SignatureException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends RuntimeException {
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handlerSecurityException(Exception exception){
+        ProblemDetail errorDetail = null;
+
+        exception.getStackTrace();
+
+        if (exception instanceof BadCredentialsException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), exception.getMessage());
+            errorDetail.setProperty("description", "The username or password is incorrect.");
+
+            return errorDetail;
+        }
+
+        if (exception instanceof AccountStatusException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
+            errorDetail.setProperty("description", "The account is locked.");
+
+            return errorDetail;
+        }
+
+        if (exception instanceof AccessDeniedException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
+            errorDetail.setProperty("description", "You are not authorized to access this resource.");
+
+            return errorDetail;
+        }
+
+        if (exception instanceof SignatureException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), exception.getMessage());
+            errorDetail.setProperty("description", "The JWT signature is invalid.");
+
+            return errorDetail;
+        }
+
+        if (exception instanceof ExpiredJWTException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), exception.getMessage());
+            errorDetail.setProperty("description", "The JWT token has expired.");
+
+            return errorDetail;
+        }
+
+        if (exception == null) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), exception.getMessage());
+            errorDetail.setProperty("description", "Unknown internal server error.");
+        }
+
+        return errorDetail;
+    }
+}
