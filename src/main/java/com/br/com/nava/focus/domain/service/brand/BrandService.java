ec1544710d445b44b9000b91874617a260e1f2bd
luger-mano <lucas.germanysantos@gmail.com>
2026-02-06 01:43:12 -0300

    [feat] : Adicionei novo método para buscar brand por id

--- src/main/java/com/br/com/nava/focus/domain/service/brand/BrandService.java
+++ src/main/java/com/br/com/nava/focus/domain/service/brand/BrandService.java
@@ -2,6 +2,7 @@ package com.br.com.nava.focus.domain.service.brand;
 
 import com.br.com.nava.focus.adapter.dto.brand.BrandRequestDto;
 import com.br.com.nava.focus.adapter.dto.brand.BrandResponseDto;
+import com.br.com.nava.focus.domain.model.Brand;
 
 import java.util.List;
 
@@ -9,4 +10,5 @@ public interface BrandService {
     BrandResponseDto createBrand(BrandRequestDto dto);
 
     BrandResponseDto getBrandById(String brandId);
+    Brand findById(String brandId);
 }
