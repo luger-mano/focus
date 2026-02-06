4ce0b67f0e345f4bcb535cb246ca948c0d3c90e4
luger-mano <lucas.germanysantos@gmail.com>
2026-02-06 01:40:23 -0300

    [feat | refact] : Criei um novo método em repository para trazer lojas de uma determinada brand por id, removi algumas anotações desnecessárias.

--- src/main/java/com/br/com/nava/focus/adapter/dto/store/StoreResponseDto.java
+++ src/main/java/com/br/com/nava/focus/adapter/dto/store/StoreResponseDto.java
@@ -1,16 +1,9 @@
 package com.br.com.nava.focus.adapter.dto.store;
 
-import com.br.com.nava.focus.domain.model.Address;
+import com.br.com.nava.focus.adapter.dto.address.AddressResponseDto;
 import com.br.com.nava.focus.domain.model.Contact;
-import com.br.com.nava.focus.domain.model.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 
-import java.util.Set;
-
-public record StoreResponseDto(@NotEmpty(message = "As informações de contato não podem estar vazias")
-                               Contact contact,
-                               @NotEmpty(message = "As informações de endereço não podem estar vazias")
-                               Address address){
+public record StoreResponseDto(Contact contact,
+                               AddressResponseDto address){
 
 }
--- src/main/java/com/br/com/nava/focus/domain/repository/StoreRepository.java
+++ src/main/java/com/br/com/nava/focus/domain/repository/StoreRepository.java
@@ -1,10 +1,14 @@
 package com.br.com.nava.focus.domain.repository;
 
 import com.br.com.nava.focus.domain.model.Store;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.UUID;
 
 public interface StoreRepository extends JpaRepository<Store, UUID> {
 
     boolean existsByContactEmail(String email);
+    Page<Store> findByBrand_BrandId(Pageable pageable, String brandId);
 }
