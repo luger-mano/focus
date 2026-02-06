c2546dc1c0b8744c70bc98a9ab0c2713dd5dbfeb
luger-mano <lucas.germanysantos@gmail.com>
2026-02-06 01:37:03 -0300

    [ feat | refact ] : Criei um novo método para buscar brand por Id, otimizei os métodos getBrandById.

--- src/main/java/com/br/com/nava/focus/domain/service/brand/BrandServiceImpl.java
+++ src/main/java/com/br/com/nava/focus/domain/service/brand/BrandServiceImpl.java
@@ -3,6 +3,7 @@ package com.br.com.nava.focus.domain.service.brand;
 
 import com.br.com.nava.focus.adapter.dto.brand.BrandRequestDto;
 import com.br.com.nava.focus.adapter.dto.brand.BrandResponseDto;
+import com.br.com.nava.focus.domain.model.Brand;
 import com.br.com.nava.focus.domain.repository.BrandRepository;
 import com.br.com.nava.focus.mapper.BrandMapper;
 import jakarta.transaction.Transactional;
@@ -10,28 +11,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.UUID;
-
 @Service
 public class BrandServiceImpl implements BrandService {
 
     private final BrandRepository brandRepository;
-    private final BrandMapper mappper;
+    private final BrandMapper brandMapper;
 
-    public BrandServiceImpl(BrandRepository brandRepository, BrandMapper mappper) {
+    public BrandServiceImpl(BrandRepository brandRepository, BrandMapper mapper) {
         this.brandRepository = brandRepository;
-        this.mappper = mappper;
+        this.brandMapper = mapper;
     }
 
     @Override
     @Transactional
     public BrandResponseDto createBrand(BrandRequestDto dto) {
         try{
-            var brandDto = this.mappper.brandRequestToEntity(dto);
+            var brandDto = this.brandMapper.brandRequestToEntity(dto);
 
             var brandMapper = brandRepository.save(brandDto);
 
-            return this.mappper.brandEntityToBrandResponseDto(brandMapper);
+            return this.brandMapper.brandEntityToBrandResponseDto(brandMapper);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -39,10 +38,22 @@ public class BrandServiceImpl implements BrandService {
 
     @Override
     public BrandResponseDto getBrandById(String brandId) {
-        var brandOpp = brandRepository.findByBrandId(brandId).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        var brandOpp = brandRepository.findByBrandId(brandId)
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
-        return this.mappper.brandEntityToBrandResponseDto(brandOpp);
+        return this.brandMapper.brandEntityToBrandResponseDto(brandOpp);
 
     }
+
+    @Override
+    public Brand findById(String brandId) {
+        return brandRepository.findById(brandId)
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+
+
+
 }
