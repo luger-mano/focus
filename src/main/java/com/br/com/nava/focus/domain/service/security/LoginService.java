23b0583a4af920f9156d3fe5f8f506d3dd2a95a3
luger-mano <lucas.germanysantos@gmail.com>
2026-02-09 21:45:07 -0300

    [feat] : Criei uma lógica para verificar se as credenciais do usuário estão corretas

--- src/main/java/com/br/com/nava/focus/domain/service/security/LoginService.java
+++ src/main/java/com/br/com/nava/focus/domain/service/security/LoginService.java
@@ -4,7 +4,5 @@ import com.br.com.nava.focus.adapter.dto.security.LoginRequestDto;
 import com.br.com.nava.focus.adapter.dto.security.LoginResponseDto;
 
 public interface LoginService {
-
     LoginResponseDto login(LoginRequestDto dto);
-
 }
