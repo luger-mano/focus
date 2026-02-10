ec46c8822c9322c786cbf3749da36dbc34e2d3a9
luger-mano <lucas.germanysantos@gmail.com>
2026-02-09 21:43:13 -0300

    [feat] : Implementei a lógica de para gerar o token JWT e devolver no corpo da requisição atráves do Dto

---
+++ src/main/java/com/br/com/nava/focus/domain/service/security/LoginService.java
@@ -0,0 +1,10 @@
+package com.br.com.nava.focus.domain.service.security;
+
+import com.br.com.nava.focus.adapter.dto.security.LoginRequestDto;
+import com.br.com.nava.focus.adapter.dto.security.LoginResponseDto;
+
+public interface LoginService {
+
+    LoginResponseDto login(LoginRequestDto dto);
+
+}
---
+++ src/main/java/com/br/com/nava/focus/domain/service/security/LoginServiceImpl.java
@@ -0,0 +1,68 @@
+package com.br.com.nava.focus.domain.service.security;
+
+import com.br.com.nava.focus.adapter.dto.security.LoginRequestDto;
+import com.br.com.nava.focus.adapter.dto.security.LoginResponseDto;
+import com.br.com.nava.focus.domain.model.Role;
+import com.br.com.nava.focus.domain.model.User;
+import com.br.com.nava.focus.domain.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class LoginServiceImpl implements LoginService {
+
+    private final JwtEncoder jwtEncoder;
+    private final UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public LoginServiceImpl(JwtEncoder jwtEncoder, UserRepository userRepository) {
+        this.jwtEncoder = jwtEncoder;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public LoginResponseDto login(LoginRequestDto dto) {
+
+        Optional<User> user = userRepository.findByEmail(dto.getEmail());
+
+        if (user.isPresent()) {
+
+            Instant now = Instant.now();
+            long expiresIn = 300L;
+
+            String scopes = user.get()
+                    .getRoles()
+                    .stream()
+                    .map(Role::getName)
+                    .collect(Collectors.joining(" "));
+
+            JwtClaimsSet claims = JwtClaimsSet.builder()
+                    .issuer("focus_java")
+                    .subject(user.get().getUserId().toString())
+                    .issuedAt(now)
+                    .expiresAt(now.plusSeconds(expiresIn))
+                    .claim("scope", scopes)
+                    .build();
+
+            String jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+            LoginResponseDto response = new LoginResponseDto();
+            response.setAccessToken(jwtValue);
+            response.setExpiresIn(expiresIn);
+
+            return response;
+        }
+
+        throw new BadCredentialsException("Email ou Senha inválidos.");
+    }
+}
