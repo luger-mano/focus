f0e1a81e74a15e2511dc663a634be6b6d5b91aeb
luger-mano <lucas.germanysantos@gmail.com>
2026-02-12 02:29:12 -0300

    [feat] : Configurei beans para serializar e desserializar objetos java

--- src/main/java/com/br/com/nava/focus/infra/config/RedisConfig.java
+++ src/main/java/com/br/com/nava/focus/infra/config/RedisConfig.java
@@ -1,4 +1,56 @@
 package com.br.com.nava.focus.infra.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
 public class RedisConfig {
+
+    @Bean
+    public ObjectMapper redisObjectMapper(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+
+        objectMapper.activateDefaultTyping(
+                objectMapper.getPolymorphicTypeValidator(),
+                ObjectMapper.DefaultTyping.NON_FINAL
+        );
+
+        return objectMapper;
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory factory,
+                                                           ObjectMapper objectMapper) {
+
+
+
+        GenericJackson2JsonRedisSerializer serializer =
+                new GenericJackson2JsonRedisSerializer(objectMapper);
+
+        RedisCacheConfiguration config =
+                RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                serializer
+                        )
+                )
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofSeconds(60));
+
+        return RedisCacheManager.builder(factory)
+                .cacheDefaults(config)
+                .build();
+    }
+
 }
