318f38022a740b2f5f24f2a89a2763a2c2c2d9b3
luger-mano <lucas.germanysantos@gmail.com>
2026-02-13 01:22:52 -0300

    [feat] : Adicionei mapeamento de alguns dto para dto e dto para entity

--- src/main/java/com/br/com/nava/focus/mapper/UserMapper.java
+++ src/main/java/com/br/com/nava/focus/mapper/UserMapper.java
@@ -1,13 +1,18 @@
 package com.br.com.nava.focus.mapper;
 
 
-import com.br.com.nava.focus.adapter.dto.security.LoginRequestDto;
+import com.br.com.nava.focus.adapter.dto.user.CreateUserRequestDto;
+import com.br.com.nava.focus.adapter.dto.user.CreateUserResponseDto;
+import com.br.com.nava.focus.adapter.dto.user.UserResponseDto;
 import com.br.com.nava.focus.domain.model.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    User LoginRequestDtoToEntity(LoginRequestDto dto);
+    CreateUserResponseDto userEntityToCreateUserResponseDto(User user);
 
+    UserResponseDto userEntityToUserResponseDto(User user);
+
+    User createUserRequestDtoToUserEntity(CreateUserRequestDto dto);
 }
