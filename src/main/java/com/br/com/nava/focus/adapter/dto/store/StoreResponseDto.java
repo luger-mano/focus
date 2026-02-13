5af83ca0376475f3611b45c3df3ade375084b5bb
luger-mano <lucas.germanysantos@gmail.com>
2026-02-13 01:18:13 -0300

    [refact] : Removi Contact da entity StoreResponseDto e criei um método para transformar dto para entity

--- src/main/java/com/br/com/nava/focus/adapter/dto/store/StoreResponseDto.java
+++ src/main/java/com/br/com/nava/focus/adapter/dto/store/StoreResponseDto.java
@@ -1,20 +1,43 @@
 package com.br.com.nava.focus.adapter.dto.store;
 
 import com.br.com.nava.focus.adapter.dto.address.AddressResponseDto;
-import com.br.com.nava.focus.domain.model.Contact;
+import com.br.com.nava.focus.domain.model.Store;
+import com.br.com.nava.focus.domain.model.User;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
 
 @Getter
 @Setter
 public class StoreResponseDto {
 
-    private Contact contac;
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String cnpj;
     private AddressResponseDto address;
 
-    public StoreResponseDto(Contact contac, AddressResponseDto address) {
-        this.contac = contac;
+    public StoreResponseDto(String phoneNumber, String email, String cnpj, AddressResponseDto address) {
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.cnpj = cnpj;
         this.address = address;
+    }
+
+    public Store toEntity(User user, UUID storeId){
+        Store store = new Store();
+
+        store.setStoreId(storeId);
+        store.setUsers(List.of(user));
+        store.setName(this.getName());
+        store.setPhoneNumber(this.getPhoneNumber());
+        store.setEmail(this.getEmail());
+        store.setCnpj(this.getCnpj());
+        store.setAddress(this.getAddress().toEntity());
+
+        return store;
     }
 
 }
