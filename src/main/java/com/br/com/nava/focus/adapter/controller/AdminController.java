6b8582ebac247993b9f4dd1be81245de8d78e482
luger-mano <lucas.germanysantos@gmail.com>
2026-02-22 01:01:38 -0300

    [refact] : Criei um novo método para ADMIN adicionar novo funcionário

--- src/main/java/com/br/com/nava/focus/adapter/controller/AdminController.java
+++ src/main/java/com/br/com/nava/focus/adapter/controller/AdminController.java
@@ -1,11 +1,13 @@
 package com.br.com.nava.focus.adapter.controller;
 
 
+import com.br.com.nava.focus.adapter.dto.employee.CreateEmployeeRequestDto;
+import com.br.com.nava.focus.adapter.dto.employee.CreateEmployeeResponseDto;
 import com.br.com.nava.focus.adapter.dto.user.CreateUserRequestDto;
 import com.br.com.nava.focus.adapter.dto.user.CreateUserResponseDto;
 import com.br.com.nava.focus.adapter.dto.user.UserResponseDto;
+import com.br.com.nava.focus.domain.service.employee.EmployeeService;
 import com.br.com.nava.focus.domain.service.user.UserService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -17,15 +19,23 @@ import java.util.UUID;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
-public class UserController {
+public class AdminController {
 
     private final UserService userService;
+    private final EmployeeService employeeService;
 
     @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     @PostMapping("/admin/store/{storeId}/user")
     public ResponseEntity<CreateUserResponseDto> createUser(@RequestBody CreateUserRequestDto dto, @PathVariable UUID storeId){
         var user = userService.createUser(dto, storeId);
         return ResponseEntity.ok(user);
+    }
+
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    @PostMapping("/admin/store/{storeId}/employee")
+    public ResponseEntity<CreateEmployeeResponseDto> createEmployee(@RequestBody CreateEmployeeRequestDto dto, @PathVariable UUID storeId){
+        CreateEmployeeResponseDto employee = employeeService.createEmployee(dto, storeId);
+        return ResponseEntity.ok(employee);
     }
 
     @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
