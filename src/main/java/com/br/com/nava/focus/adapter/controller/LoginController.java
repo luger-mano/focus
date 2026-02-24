f6e856bd543973099f051543e5fcd3b99282b224
luger-mano <lucas.germanysantos@gmail.com>
2026-02-24 01:06:06 -0300

    [feat] : Adicionei o método POST signup para criar novos usuários na plataforma

--- src/main/java/com/br/com/nava/focus/adapter/controller/LoginController.java
+++ src/main/java/com/br/com/nava/focus/adapter/controller/LoginController.java
@@ -3,19 +3,32 @@ package com.br.com.nava.focus.adapter.controller;
 
 import com.br.com.nava.focus.adapter.dto.security.LoginRequestDto;
 import com.br.com.nava.focus.adapter.dto.security.LoginResponseDto;
+import com.br.com.nava.focus.adapter.dto.user.CreateUserRequestDto;
+import com.br.com.nava.focus.adapter.dto.user.CreateUserResponseDto;
 import com.br.com.nava.focus.domain.service.security.LoginService;
+import com.br.com.nava.focus.domain.service.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 public class LoginController {
 
+    private final UserService userService;
     private final LoginService loginService;
+
+    @PostMapping("/store/{storeId}/user")
+    public ResponseEntity<CreateUserResponseDto> signup(@RequestBody CreateUserRequestDto dto, @PathVariable UUID storeId){
+        var user = userService.createUser(dto, storeId);
+        return ResponseEntity.ok(user);
+    }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(@RequestBody @Valid LoginRequestDto dto){
