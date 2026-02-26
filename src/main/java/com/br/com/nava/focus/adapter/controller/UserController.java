976400efbc35f27be17da59f8c94e2bf4a457f84
luger-mano <lucas.germanysantos@gmail.com>
2026-02-26 01:22:16 -0300

    [feat] : CRUD na controller UserController, adicionei GET e DELETE

--- src/main/java/com/br/com/nava/focus/adapter/controller/UserController.java
+++ src/main/java/com/br/com/nava/focus/adapter/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.br.com.nava.focus.adapter.controller;
 
 import com.br.com.nava.focus.adapter.dto.user.CreateUserRequestDto;
+import com.br.com.nava.focus.adapter.dto.user.CreateUserResponseDto;
+import com.br.com.nava.focus.adapter.dto.user.UserInformationDto;
 import com.br.com.nava.focus.adapter.dto.user.UserResponseDto;
 import com.br.com.nava.focus.domain.service.user.UserService;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +19,28 @@ public class UserController {
 
     private final UserService userService;
 
+    @GetMapping("/user/dataUser")
+    public ResponseEntity<UserInformationDto> getUserInformation(@RequestParam("userId") UUID userId){
+        var userInformation = userService.getUserInformation(userId);
+        return ResponseEntity.ok(userInformation);
+    }
+    @PostMapping("/store/{storeId}/user")
+    public ResponseEntity<CreateUserResponseDto> signup(@RequestBody CreateUserRequestDto dto, @PathVariable UUID storeId){
+        var user = userService.createUser(dto, storeId);
+        return ResponseEntity.ok(user);
+    }
+    @PreAuthorize("hasAuthority('SCOPE_SUPER_ADMIN') or hasAuthority('SCOPE_ADMIN') or hasAuthority('SCOPE_CLIENT')")
     @PutMapping("/store/{storeId}/user/{userId}/update")
-    public ResponseEntity<UserResponseDto> updateUserDate(@RequestBody CreateUserRequestDto dto, @PathVariable UUID userId, @PathVariable UUID storeId){
+    public ResponseEntity<UserResponseDto> updateUser(@RequestBody CreateUserRequestDto dto, @PathVariable UUID userId, @PathVariable UUID storeId){
         var userUpdated = userService.updateUser(dto, userId, storeId);
         return ResponseEntity.ok(userUpdated);
+    }
+
+    @PreAuthorize("hasAuthority('SCOPE_SUPER_ADMIN') or hasAuthority('SCOPE_ADMIN') or hasAuthority('SCOPE_CLIENT')")
+    @DeleteMapping("/user/delete")
+    public ResponseEntity<Void> deleteUser(@RequestParam("userId") UUID userId){
+        userService.deleteUser(userId);
+        return ResponseEntity.ok().build();
     }
 
 }
